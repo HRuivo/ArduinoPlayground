e7933852c33124794efd74f786b0f676dfca14f1
Hugo Ruivo <hugoruivo89@gmail.com>
2021-08-26 22:03:59 +0100

    Create AnalogRead.ino

---
+++ AnalogRead.ino
@@ -0,0 +1,21 @@
+const int readPin = A0;
+int V2 = 0;
+
+float convertToVoltage(int readValue) {
+  return (5./1023.)*(float)readValue;
+}
+
+void setup() {
+  pinMode(readPin, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  V2 = analogRead(readPin);
+
+  Serial.print("Voltage 2: ");
+  Serial.println(convertToVoltage(V2));
+
+  delay(500);
+}
